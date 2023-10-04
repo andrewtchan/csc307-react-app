af6ecee3cb85232a331b8370fe69c9ab79817582
Andrew Chan <andrew2020chan@gmail.com>
2023-10-04 16:46:11 -0700

    /users route

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
