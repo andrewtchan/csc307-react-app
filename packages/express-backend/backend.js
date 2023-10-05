116d2b9b6332449b17c11674a2fb28df6bf7a26b
Andrew Chan <andrew2020chan@gmail.com>
2023-10-04 22:58:49 -0700

    feat: /users/:id GET endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -20,6 +20,16 @@ app.get("/users", (req, res) => {
   }
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
@@ -27,6 +37,9 @@ app.listen(port, () => {
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 const users = {
   users_list: [
