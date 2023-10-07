bb30448308f7b84ce477e5a691e7a999f0c363e7
Andrew Chan <andrew2020chan@gmail.com>
2023-10-06 21:34:12 -0700

    feat: get users by name and job query

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -11,8 +11,13 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+  if (name != undefined && job == undefined) {
     let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
     result = { users_list: result };
     res.send(result);
   } else {
@@ -52,6 +57,12 @@ app.listen(port, () => {
 
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
 };
 
 const findUserById = (id) =>
