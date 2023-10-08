e807d478cd96c3989f5fa22d11cd747f3d04619c
Andrew Chan <andrew2020chan@gmail.com>
2023-10-08 12:23:46 -0700

    chore: added Express cors library

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
